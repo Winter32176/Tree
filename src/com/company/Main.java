5b419279c274982c34cea96d6561dcb60c916473
rober <robert321766@outlook.com>
2020-02-09 20:16:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter width:");
+        int TreeWidth = scan.nextInt();
+
+        int howmanystars = 0;
+        int howmanyspaces = TreeWidth;
+
+
+        for (int i = 0; i < TreeWidth; i++) {
+
+            howmanyspaces--;
+
+
+            howmanystars++;
+
+
+            for (int j = 0; j < howmanyspaces; j++) {
+                System.out.print(" ");
+            }
+
+
+            for (int z = 0; z < howmanystars; z++) {
+                System.out.print(" *");
+            }
+
+
+            System.out.println();
+        }
+
+
+    }
+}
